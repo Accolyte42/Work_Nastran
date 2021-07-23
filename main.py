eff9df5831c7b4c00e58bce47360df1046364ab5
Accolyte42 <70057185+Accolyte42@users.noreply.github.com>
2021-07-23 21:21:45 +0300

    + Почистил код, добавил комментарии

--- main.py
+++ main.py
@@ -11,29 +11,31 @@ import matplotlib.pyplot as plt
 # %matplotlib qt
 
 
-
-def get_points(filename, pnt_lst='all'):
-    Flut_flag = False
+def get_points(filename, clear_columns=True):
+    # Функция, принимающая (полное) имя файла, и выдающая словарь со всеми
+    Flut_flag = False  # Флаг, отвечающий за начало расчета на флаттер в f06 файле
     count_point = 3  # номер строки, с которой идет описание случая
     count_data = 5  # номер строки, с которой идут данные (с шапкой)
     counter_row = 0  # счетчик строк для определения информативных
-    counter_points = 1  # счетчик расчетных точек
     points_temp = []  # Расчетные точки. Каждая расчетная точка - двумерный массив
     points_name_subcase = []
+    pnt_lst='all'
 
+    # После чтения всего файла получаем points_temp - это все расчетные точки подряд для всех махов (subcase-ов)
     with open(filename) as file:
         for line in file:
             line = line.strip()  # убирание лишних пробелов с начала и конца
 
-            if 'FLUTTER' and 'SUMMARY' in line:  # если встретили нужный блок
-                Flut_flag = True
+            if 'FLUTTER' and 'SUMMARY' in line:  # если встретили нужный блок с началом флаттера
+                Flut_flag = True  # Если начался флаттерная часть
                 counter_row = 1  # теперь мы можем считать строки в информативном блоке
                 pnt = []  # текущая точка
 
+            # Пока мы в нужном блоке
             while Flut_flag and (counter_row == count_point or counter_row > count_data):  # Пока мы в нужном блоке
                 if 'NASTRAN' and 'AEROELASTIC' in line:  # Определение условия выхода из блока
                     Flut_flag = False
-                    points_temp.append(pnt)
+                    points_temp.append(pnt)  #
                     counter_row = 0
                     break
 
@@ -42,7 +44,7 @@ def get_points(filename, pnt_lst='all'):
                 line += ' '  # чтобы чтение последнего слова работало
 
                 lst_str = []  # Список, в котором содержатся отдельные слова и числа из строки
-                temp_str = ''  # список в котором хранится текущее слово
+                temp_str = ''  # строка, в которой хранится текущее слово
                 for i in range(len(line)):
                     if line[i] != ' ':  # Если не конец слова, то собираем всё в текущую подстроку
                         temp_str += line[i]
@@ -57,42 +59,33 @@ def get_points(filename, pnt_lst='all'):
                 if counter_row == count_point:  # для первой строки в блоке запись нужной информации
                     lst_str = [l[0] + l[1] + l[2] + ', ' + l[3] + '_' + l[4] + l[5] + l[6]]
 
-                pnt.append(
-                    lst_str)  # прочитали всю строку, значит теперь можно список lst_str записать в текущую точку pnt
+                pnt.append(lst_str)  # прочитали всю строку, теперь можно список lst_str записать в текущую точку pnt
 
                 break
             counter_row += 1
 
-    points = []
-    cur_Mach = points_temp[0][0][0][points_temp[0][0][0].index('MACH_NUMBER'):]
+    # Далее надо разделить points_temp по сабкейсам (разным махам)
+    points = []  # 'nj e;t
+    cur_mach = points_temp[0][0][0][points_temp[0][0][0].index('MACH_NUMBER'):]
     dict_points = {}
-    # print(cur_Mach)
 
-    if pnt_lst == 'all':  # Если вывод всех тонов
-        for i in range(len(points_temp)):
-            # print(i)
-            if points_temp[i][0][0][points_temp[i][0][0].index('MACH_NUMBER'):] == cur_Mach:
-                points.append(pd.DataFrame(points_temp[i][2:], columns=points_temp[i][1], dtype=float))
-                points_name_subcase.append(points_temp[i][0])
-                cur_Mach = points_temp[i][0][0][points_temp[i][0][0].index('MACH_NUMBER'):]
-            else:
-                dict_points[float(cur_Mach[12:])] = points
-                # print(cur_Mach)
-                cur_Mach = points_temp[i][0][0][points_temp[i][0][0].index('MACH_NUMBER'):]
-                points = []
-                points_name_subcase = []
-            if i == len(points_temp) - 1:
-                dict_points[float(points_temp[i][0][0][points_temp[i][0][0].index('MACH_NUMBER') + 12:])] = points
-                # print(cur_Mach)
-
-    else:  # Если ввод определенных тонов
-        for i in pnt_lst:
+    # Далее неразделенные записи points_temp деляться на записи по сабкейсам в словарь
+    for i in range(len(points_temp)):  # прохождение неразделенных на сабкейсы записей.
+        if points_temp[i][0][0][points_temp[i][0][0].index('MACH_NUMBER'):] == cur_mach:  # Если сабкейсы не изменился
             points.append(pd.DataFrame(points_temp[i][2:], columns=points_temp[i][1], dtype=float))
-            points_name_subcase.append(points_temp[i][0])
+            cur_mach = points_temp[i][0][0][points_temp[i][0][0].index('MACH_NUMBER'):]
+        else: # Если сабкейсы изменился, значит надо сохранить всё, что накопилось в прошлом сабкейсе
+            dict_points[float(cur_mach[12:])] = points
+            cur_mach = points_temp[i][0][0][points_temp[i][0][0].index('MACH_NUMBER'):]
+            points = []
+        if i == len(points_temp) - 1:  # Обработка самого последнего сабкейса. Т.к. для последнего нет изменения
+            dict_points[float(points_temp[i][0][0][points_temp[i][0][0].index('MACH_NUMBER') + 12:])] = points
 
-    for key in dict_points:
-        for df in dict_points[key]:
-            df.drop(['KFREQ', '1./KFREQ', 'COMPLEX', 'EIGENVALUE'], axis=1, inplace=True)
+    # Убирание лишних колонок
+    if clear_columns:
+        for key in dict_points:
+            for df in dict_points[key]:
+                df.drop(['KFREQ', '1./KFREQ', 'COMPLEX', 'EIGENVALUE'], axis=1, inplace=True)
 
     return dict_points
 
@@ -100,13 +93,9 @@ def get_points(filename, pnt_lst='all'):
 dct = get_points('rv_30_surf_all_fl_m085_089.f06')
 
 print(dct.keys())
-points = dct[0.85]
+points = dct[0.89]
 print(points[0].columns)
-# del points[0]['KFREQ'], points[0]['1./KFREQ'], points[0]['COMPLEX'], points[0]['EIGENVALUE']
 print(points[0])
-# print(len(dct))
-# print(len(points))
-# print(len(points[0]) // 2)
 
 
 # for i in dct:  # Проход по всем Махам
@@ -125,4 +114,5 @@ for i in range(len(points)):
 plt.grid()
 plt.show()
 
-# Надо добавить оформление получше, выбор файла и выбор точек. Всё остальное запрятать
+
+
